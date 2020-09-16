abb59e480cd4c2a603098be6d2fd0029e551612e
Thugmek <tomekmichalhu@seznam.cz>
2020-09-16 21:52:19 +0200

    Ještě kosmetické úpravy

--- src/App.java
+++ src/App.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        /**
+         * Zde lze nastavit počet výpočetních vláken. Mám 8 jader procerou, proto 8. Zkoušel jsem i 4, 16 a 32, ale 8
+         * je na mém hardwaru výrazně nejrychlejší volba. Procesro je vytěžován na 25%, bottleneck jerychlost operační
+         * paměťi :/
+         */
         Factorial f = new Factorial(8);
         Scanner sc = new Scanner(System.in);
         try {
@@ -26,6 +31,7 @@ public class App {
             }
             inputReader.close();
             writer.close();
+            System.out.println("Výsledek uložen do output.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
